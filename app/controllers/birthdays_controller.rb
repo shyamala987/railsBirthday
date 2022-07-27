77fca54f2a06e42501a7f60b48ac6d2bd5efe906
Shyamala Venkataramani <shyamala.ramani@gmail.com>
2022-07-26 19:15:47 -0700

    crud routes

--- app/controllers/birthdays_controller.rb
+++ app/controllers/birthdays_controller.rb
@@ -6,4 +6,23 @@ class BirthdaysController < ApplicationController
   def show
     @birthday = Dob.find(params[:id])
   end
+
+  def new
+    @birthday = Dob.new
+  end
+
+  def create
+    @birthday = Dob.new(name: "...", birthday: "...")
+
+    if @birthday.save
+      redirect_to @birthday
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def birthday_params
+      params.require(:birthday).permit(:name, :birthday)
+    end
 end
--- app/models/dob.rb
+++ app/models/dob.rb
@@ -1,2 +1,4 @@
 class Dob < ApplicationRecord
+    validates :name, presence: true
+    validates :birthday, presence: true, length: { is: 10 }
 end
