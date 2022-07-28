4de466ee234709d67ba1f95e57271b600f964f79
Shyamala Venkataramani <shyamala.ramani@gmail.com>
2022-07-27 18:14:46 -0700

    conflict resolution

--- db/migrate/20220728010410_create_birthdays.rb
+++ db/migrate/20220728010410_create_birthdays.rb
@@ -2,7 +2,7 @@ class CreateBirthdays < ActiveRecord::Migration[7.0]
   def change
     create_table :birthdays do |t|
       t.string :name
-      t.string :birthday
+      t.text :dob
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_24_045402) do
-  create_table "dobs", force: :cascade do |t|
+ActiveRecord::Schema[7.0].define(version: 2022_07_28_010410) do
+  create_table "birthdays", force: :cascade do |t|
     t.string "name"
-    t.string "birthday"
+    t.text "dob"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
